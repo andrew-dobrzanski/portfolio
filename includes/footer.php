24473cf17c3367c0268b30a16c4c539f7f02d9a6
Andrew Dobrzanski <dobrzanski.andrew@gmail.com>
2017-02-08 22:26:43 -0800

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,94 @@
+<!-- footer -->
+<footer>
+	<!-- footer wrapper -->
+	<div id="footer-wrapper" class="footer dark container-fluid">
+		<div class="container">
+			<div class="row">
+				<!-- Column 1 -->
+				<article class="col-md-4 col-sm-4 col-xs-12">
+					<div class="">
+						<h2><small>Important Info</small></h2>
+					</div>
+					<p>Passionate Front-end Web Developer specializing in UI/UX Design. I have pixel perfect attention to detail with a hunger for problem solving and constantly perfecting my craft.</p>
+				</article>
+				<!-- Column 2 -->
+				<article class="col-md-4 col-sm-4 col-xs-12">
+					<div class="">
+						<h2><small>Quick Links</small></h2>
+					</div>
+					<ul class="list-unstyled link-list">
+						<li><a href="./index.php">Home</a></li>
+						<li><a href="./about.php">About</a></li>
+						<li><a href="./web-development.php">Portfolio</a></li>
+						<li><a href="./contact.php">Contact</a></li>
+					</ul>
+				</article>
+				<!-- Column 3 -->
+				<!-- <article class="col-md-3 col-sm-3 col-xs-12">
+					<div class="">
+						<h2><small>Latest Posts</small></h2>
+					</div>
+					<ul class="list-unstyled link-list">
+						<li><a href="">Blog Post 1<small>Nov 16, 2015</small></a></li>
+						<li><a href="">Blog Post 2<small>Oct 5, 2015</small></a></li>
+						<li><a href="">Blog Post 3<small>Aug 22, 2015</small></a></li>
+					</ul>
+				</article> -->
+				<!-- Coulmn 4 -->
+				<article class="col-md-4 col-sm-4 col-xs-12">
+					<div class="">
+						<h2><small>Contact Info</small></h2>
+					</div>
+					<p>Portland, Oregon<br/>
+					Phone: 971.280.4560<br/>
+					Email: <a href="mailto:dobrzanski.andrew@gmail.com">dobrzanski.andrew@gmail.com</a>
+					</p>
+				</article>
+			</div>
+		</div>
+	</div><!-- end footer-wrapper -->
+</footer><!-- end footer -->
+
+<!-- Copyright text -->
+<div class="copyright dark">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<p>2017 All Rights Reserved. <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></p>
+			</div>
+			<!-- Social links -->
+			<div class="col-md-6">
+				<ul class="footer-socials list-inline right">
+					<!-- Linkedin -->
+					<li>
+						<a href="http://www.linkedin.com/in/davidandrewdobrzanski" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Linkedin" target="_blank">
+							<i class="fa fa-linkedin"></i>
+						</a>
+					</li>
+					<!-- Facebook -->
+					<li>
+						<a href="https://www.facebook.com/andrew.dobrzanski.71" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Facebook" target="_blank">
+							<i class="fa fa-facebook"></i>
+						</a>
+					</li>
+					<!-- Instagram -->
+					<li>
+						<a href="https://www.instagram.com/andrew_dobrzanski/" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Instagram" target="_blank">
+							<i class="fa fa-instagram"></i>
+						</a>
+					</li>
+					<!-- Twitter -->
+					<li>
+						<a href="https://twitter.com/A_Dobrzanski" class="tooltips" data-toggle="tooltip" data-placement="top" title="" data-original-title="Twitter" target="_blank">
+							<i class="fa fa-twitter"></i>
+						</a>
+					</li>
+				</ul>
+			</div><!-- end social media -->
+		</div>
+	</div>
+</div><!-- end .copyright -->
+</div><!-- end #main -->
+
+</body>
+</html>
